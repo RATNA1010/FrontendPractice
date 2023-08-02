c50bdf955f22ed4d9fa8b748b52d49add4f32726
Ratna <ratnababu480@gmail.com>
2023-08-02 12:53:08 +0530

    Javascript basics

---
+++ 08/01/2023/index.js
@@ -0,0 +1,24 @@
+
+var a = "Ratna";
+console.log("Data type of a is: ", typeof a);
+
+var n = 10;
+console.log("Data type of n is: ", typeof n);
+
+var b = true;
+console.log("Data type of b is: ", typeof b); 
+
+var e = null;
+console.log("Data type of e is: ", typeof e);
+
+var u;
+console.log("Data type of u is: ", typeof u);
+
+var obj = {}
+console.log("Data type of obj is: ", typeof obj);
+
+var arr = [1,2,3,4]
+console.log("Data type of arr is: ", typeof arr);
+
+var f = function(){}
+console.log("Data type of f is: ", typeof f);
