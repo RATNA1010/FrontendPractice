676f5c8ff2becc3d1f4fdb032f564a5cab760397
Ratna <ratnababu480@gmail.com>
2023-08-03 20:03:52 +0530

    "Js Destructure and spread"

---
+++ index.js
@@ -0,0 +1,77 @@
+
+// var arr1 = [1, 2, 3];
+// var arr2 = [4, 5, 6];
+
+// const result = [...arr1, ...arr2];
+
+// console.log("result:", result);
+
+
+// var ayodhya = ["Srirama", "Lakshmana", "Bharata", "Shatrugna"];
+
+// var midhila = ["Sita", "Urmila", "Mandavi", "Shrutakeerthi"];
+
+// for (var i = 0; i < midhila.length; i++) {
+//     ayodhya.push(midhila[i]);
+// }
+
+// console.log("Final:", ayodhya);
+
+/* const ramayan = [...ayodhya, ...midhila];
+console.log(ramayan);
+
+var [king, , , , queen, ...rest] = ramayan;
+console.log("king:", king);
+console.log("queen:", queen);
+console.log("Sisters:", rest); */
+
+//for loop
+
+// var names = ["Ratan", "Govind", "Babu"];
+
+// for(var i = 0; i < names.length; i++) {
+//     if(names[i] == "Ratan"){
+
+//     console.log("name is: ", names[i]);
+//     }
+// }
+
+
+//Map method
+
+var games = ["Vollyball", "Cricket", "Kabaddi", "Football"];
+
+// var result = games.map(function(sport){
+//     if(sport == "Cricket"){
+//     return sport;
+//     }
+//     return null;
+// }).filter(function(value){
+//     return value !==null;
+// });
+
+// console.log(result);
+
+
+//Filter method
+
+// var filteredGames = games.filter(function(sport){
+//     return sport !== "Cricket";
+// });
+
+// console.log(filteredGames);
+
+var epic1 = "Ramayanam";
+
+var epic2 = "Mahabharatam";
+
+// console.log(epic.length);
+
+// console.log(epic.indexOf("y"));
+
+// console.log(epic1+" "+epic2);
+
+// console.log(epic1.concat(" ").concat(epic2));
+
+
+
