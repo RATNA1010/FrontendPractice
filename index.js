dc919f923a25374b8eb6f5ad5acb37342533982c
Ratna <ratnababu480@gmail.com>
2023-08-07 18:56:53 +0530

    08-07-23

--- index.js
+++ index.js
@@ -1,93 +1,46 @@
-//Objects
+// Functions
 
-//Object literals
+console.log("value of a is: ", a); //Hoisting - accessing variables top of declaration will result "undefined" instead of error because of hoisting
 
-var Employee = {
-    fName: "Ratan",
-    lName: "babu",
-    id : 101,
-    city: "Hyderabad",
-    dept: function(){
-        console.log("i am function inside object");
-    },
-    car: {
-        company: "Audi",
-        model: "A6",
-        colour: "White"
-    }
+var a = 10;
+var b = "Ratan";
+var arr = [1, 2, 3, 4, 5];
+
+// console.log("value of a is: ", a);
+// console.log("value of b is: ", b);
+// console.log("value of arr is: ", arr);
+
+
+//ES5 - let, const are function/local/block scoped  
+function myFunction() {
+    var i = 11;
+    console.log("value of a is: ", a); //10
+    console.log("value of b is: ", b); //Ratan
+    console.log("value of arr is: ", arr); //[1, 2, 3, 4, 5]
+    console.log("value of i is: ", i); // 11
+}
+myFunction();
+// console.log("value of i is: ", i); // we can't access the i variable outside function because it is function scoped variable 
+
+//const(immutable - can't change the value after assigned)
+// const str = "Ratan";
+
+// str = "Goviind"; //cant,t change const value
+
+// console.log(str);
+
+for(let i = 0; i < 5; i++){
+    console.log(i);
 }
 
-// console.log(Object.keys(Employee));
+//console.log(i); // can't access i since it is block scoped
 
-  
+function myExample(user, city){
 
-// console.log(Employee.car.colour);
-
-// console.log(Employee.car["model"]);
-// console.log(Employee.fName);
-// console.log(Employee.city);
-
-// console.log(Employee.id);
-
-Employee.gender = "Male";
-// console.log(Employee);
-
-// console.log(Employee.dept());
-
-var {fName, lName, city, id} = Employee;
-
-console.log(fName, city, gender = "male");
-
-//Object constructor function
-
-function Person(user, city){
-    this.name = user;
-    this.city = city;
-    this.email = user+"@abc.com";
+    console.log(arguments)
+    console.log("Name is: ", user);
+    console.log("City is: ", city);
 }
+myExample("Ratan", "HYD");
 
-var emp_1 = new Person("Ratan", "HYD");
 
-var emp_2 = new Person("Yash", "BLR");
-
-// var clg_2 = new College("University", "Sambala", 6);
-
-// console.log(emp_1);
-
-// console.log("person details: ", emp_2);
-
-var User = {
-    fName: "test",
-    age: 30,
-    city: "hyd"
-
-}
-
-let a = "fName";
-
-// console.log(User.a)
-// console.log(User["fName"])
-
-var Example = [{
-    name: "test1",
-    id:10,
-    age: 21
-},
-{
-    name: "test2",
-    id: 11,
-    age: 22
-},
-{
-    name: "test3",
-    id: 12,
-    age: 22
-}]
-
-console.log(Example);
-
-var result = Example.filter(function(value){
-    return value.age == 22;
-  });
-
-console.log(result)
